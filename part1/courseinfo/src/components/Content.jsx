54c82f2f6e7d5b09c463e0e0ed31ef2369f563c8
Osaro Iyoha <saro.iyoha@gmail.com>
2023-09-27 18:08:56 +0100

    exercises 1.3 to 1.5

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Part from './Part'
 
-function Content({part1, exercises1, 
-    part2, exercises2, part3, exercises3}) {
+function Content({parts}) {
   return (
     <div>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
+      <Part part={parts[0].name} exercises={parts[0].exercises}/>
+      <Part part={parts[1].name} exercises={parts[1].exercises}/>
+      <Part part={parts[2].name} exercises={parts[2].exercises}/>
     </div>
   )
 }
